c4a14c5e32c057f89d1a0fb326b3d100402ce753
aplqo <aplqo@outlook.com>
2020-01-24 22:26:13 +0800

    Update debug scripts for autodiff tool

--- debug_scripts/testall.sh
+++ debug_scripts/testall.sh
@@ -2,6 +2,9 @@
 
 inreg='in$'
 ansreg='ans$'
+readonly test="-test 7 diff [output] [answer] -a -w --color -u"
+readonly limfiles="-files 2 [output] [answer]"
+readonly diff="-diff [input].diff"
 
 if [ -n "$4" ]
 then
@@ -11,4 +14,4 @@ if [ -n "$5" ]
 then
     ansreg="$5"
 fi
-./bin/group $1 -test-regex "$3" -indir "$2" -in-regex "$inreg" -ansdir "$2" -ans-regex "$ansreg" -test diff -testargs 6 [output] [answer] -a -w --color -u
+./bin/group $1 -test-regex "$3" -indir "$2" -in-regex "$inreg" -ansdir "$2" -ans-regex "$ansreg" -test '.\bin\diff' -testargs 15 $limfiles $diff $test
