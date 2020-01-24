812dc2f056914147c7d7e8ae334db9bd5cab6ad0
aplqo <aplqo@outlook.com>
2020-01-24 17:49:42 +0800

    Update testone.sh for linux

--- debug_scripts/testone.sh
+++ debug_scripts/testone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/single.exe $1 -in $2.in -out $2.out $3 -test diff -testargs 6 $2.out $2.ans -a -w --color -u
+./bin/single $1 -in $2.in -out $2.out $3 -test diff -testargs 6 $2.out $2.ans -a -w --color -u
