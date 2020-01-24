96bf2083a0b0c74338d02e14c51a0a4b0e1e69e8
aplqo <aplqo@outlook.com>
2020-01-24 18:36:59 +0800

    Correct some mistakes

--- src/autodiff.cpp
+++ src/autodiff.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <filesystem>
 #include <iostream>
+#include <sstream>
 #include <string>
 using namespace std;
 using namespace apdebug::out;
@@ -11,22 +12,30 @@ using namespace apdebug::info;
 using namespace std::filesystem;
 
 uintmax_t lim = 200;
-bool red = false;
+bool red = false, silent = false;
+ostringstream nul;
 string cmd;
 
 int main(int argc, char* argv[])
 {
+    if (!strcmp(argv[1], "-quiet"))
+    {
+        silent = true;
+        cerr.rdbuf(nul.rdbuf());
+    }
     cerr << "Aplqo debug tool: auto diff" << endl;
     cerr << "Version git@" << apdebug::info::hash << " " << apdebug::info::version << endl;
     cerr << "Build branch: " << apdebug::info::branch << endl;
     cerr << "Build on " << __TIME__ << " " << __DATE__ << " by " << apdebug::info::builder << endl;
 
     cerr << col::CYAN;
+
     for (int i = 1; i < argc; ++i)
     {
         if (!strcmp(argv[i], "-limit"))
         {
             lim = stoull(argv[++i]);
+            cerr << "Autodiff: File size limit: " << lim << " byte." << endl;
             continue;
         }
         if (!strcmp(argv[i], "-files"))
@@ -40,10 +49,10 @@ int main(int argc, char* argv[])
                     cerr << col::RED << "Autodiff: can't find file " << f << endl;
                     return 1;
                 }
-                if (file_size(f) > lim || (!red))
+                if (file_size(f) > lim && (!red))
                 {
                     red = true;
-                    cerr << "Autodiff: File " << f << " tpp large, redirect stdout to file." << endl;
+                    cerr << "Autodiff: File " << f << " too large, redirect stdout to file." << endl;
                 }
             }
             continue;
@@ -51,7 +60,7 @@ int main(int argc, char* argv[])
         if (!strcmp(argv[i], "-diff"))
         {
             ++i;
-            if (red)
+            if (red || silent)
             {
                 cerr << "Autodiff: Redirect stdout to " << argv[i] << endl;
                 freopen(argv[i], "w", stdout);
@@ -62,12 +71,14 @@ int main(int argc, char* argv[])
             unsigned long n = stoul(argv[++i]);
             for (unsigned int j = 0; j < n; ++j)
                 cmd = cmd + " \"" + argv[++i] + "\"";
-            cerr << "Autodiff: Test command " << cmd << endl;
+            cerr << col::BLUE << "Autodiff: Test command " << cmd << endl;
         }
     }
-    cerr << col::NONE << endl;
+    if (!red)
+        cerr << col::NONE << endl;
     int ret = system(cmd.c_str());
-    cerr << endl;
+    if (!red)
+        cerr << endl;
     if (ret)
         cerr << col::YELLOW;
     else
