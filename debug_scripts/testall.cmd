6d7c2fed935f750013ed8b6d3d0c4ba57638beb2
aplqo <aplqo@outlook.com>
2020-01-29 21:40:27 +0800

    Correct mistake in testall.cmd

--- debug_scripts/testall.cmd
+++ debug_scripts/testall.cmd
@@ -1,9 +1,6 @@
 @echo off
 
-inreg="in$"
-ansreg="ans$"
+if "%4"=="" set inreg=in$ else set inreg="%4"
+if "%5"=="" set ansreg=ans$ else set ansreg="%5"
 
-if not %4 == "" inreg=%4
-if not %5 == "" ansreg=%5
-
-bin\group %1 -test-regex "%3" -indir "%2" -in-regex "%inreg" -ansdir "%2" -ans-regex "%ansreg" -test '.\scripts\test.cmd' -testargs 3 [input] [output] [answer]
+bin\group %1 -test-regex "%3" -indir "%2" -in-regex %inreg% -ansdir %2 -ans-regex %ansreg% -test .\scripts\test.cmd -testargs 3 [input] [output] [answer]
