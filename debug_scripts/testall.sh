d79e094a6b99fd52be1feac1e5c417206d186c98
aplqo <aplqo@outlook.com>
2020-02-02 21:22:49 +0800

    Correct some mistakes in debug scripts

--- debug_scripts/testall.sh
+++ debug_scripts/testall.sh
@@ -11,4 +11,4 @@ if [ -n "$5" ]
 then
     ansreg="$5"
 fi
-./bin/group $1 -test-regex "$3" -indir "$2" -in-regex "$inreg" -ansdir "$2" -ans-regex "$ansreg" -test '.\scripts\test.sh' -testargs 3 [input] [output] [answer]
+./bin/group $1 -test-regex "$3" -indir "$2" -in-regex "$inreg" -ansdir "$2" -ans-regex "$ansreg" -test './scripts/test.sh' -testargs 3 [input] [output] [answer]
