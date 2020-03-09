61618a67b1139f6067993ced6c13128cb835b54b
aplqo <aplqo@outlook.com>
2020-03-09 23:41:15 +0800

    Add shared directory

---
+++ debug_config/Windows/shared/README-Windows.txt
@@ -0,0 +1,1 @@
+This folder contains shared files on Windows system.
--- debug_config/shared/vscode/init-vscode.cmd
+++ debug_config/shared/vscode/init-vscode.cmd
@@ -1,12 +1,8 @@
 set prefix=.\config\vscode
 mkdir "%1\.vscode"
+echo "%2" > "%1\.config\vscode"
 if exist "%preifx%\%2\%2-init.cmd" (
     %preifx%\%2\%2-init.cmd "%1"
 ) else (
     copy "%prefix%\%2\*" "%1\.vscode\"
-)
-echo "%2" > "%1\.config\vscode"
-if exist "%prefix%\%2\%2-deinit.cmd" (
-    copy "%prefix%\%2-deinit.cmd" "%1\.dtors"
-    del /s /q "%1\scripts\%2-deinit.cmd"
 )
