f90d02623bf2cf3c615a56c0e8f067f9622f56b3
aplqo <aplqo@outlook.com>
2020-04-15 18:44:17 +0800

    Correct some mistake in generator.h

--- debug_tools/generator.h
+++ debug_tools/generator.h
@@ -3,19 +3,26 @@
 
 #include <random>
 
-namespace apdebug::generator
+namespace apdebug
 {
-    std::mt19937_64 default_rnd(std::random_device().operator());
-    template <class T, class R>
-    T GetRandom(const T a, const T b, R rnd = default_rnd) // get random number once
+    namespace generator
     {
-        std::uniform_int_distribution<T> dis(a, b);
-        return dis(rnd);
-    }
-    template <class T>
-    T MayBe(const T val, const T def)
-    {
-        return val ? val : def;
+        std::mt19937_64 default_rnd;
+        void Init()
+        {
+            default_rnd.seed(std::random_device()());
+        }
+        template <class T, class R = std::mt19937_64>
+        T GetRandom(const T a, const T b, R rnd = default_rnd) // get random number once
+        {
+            std::uniform_int_distribution<T> dis(a, b);
+            return dis(rnd);
+        }
+        template <class T>
+        T MayBe(const T val, const T def)
+        {
+            return val ? val : def;
+        }
     }
 } // namespace apdebug
 
