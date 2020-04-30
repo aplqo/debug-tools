263fc3e1554325990346ca2fedf9ae18f1251c16
aplqo <aplqo@outlook.com>
2020-05-01 00:07:24 +0800

    Add basic file for init

---
+++ include/init.h
@@ -0,0 +1,142 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include "include/output.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+namespace apdebug::init
+{
+    // simple file operations
+    template <class T>
+    void writeFile(const std::filesystem::path& p, const T& dat)
+    {
+        std::ofstream f(p);
+        f << dat;
+        f.close();
+    }
+    template <class T>
+    T readFileVal(const std::filesystem::path& p)
+    {
+        std::ifstream f(p);
+        T ret;
+        f >> ret;
+        return ret;
+    }
+    std::string readFileLn(const std::filesystem::path& p);
+
+    template <class T>
+    class list
+    {
+    public:
+        list(const char* typ)
+            : tab({ "Id", typ, "Description" })
+        {
+        }
+        void append(T c)
+        {
+            m[c->name] = size();
+            lst.push_back(c);
+            tab.update(1, c->name.length());
+            tab.update(2, c->description.length());
+        }
+        void print(unsigned int beg = 0) const
+        {
+            tab.header(std::cout);
+            std::cout << std::endl;
+            for (auto i : lst)
+            {
+                tab.print(0, beg, std::cout);
+                tab.print(1, i->name, std::cout);
+                tab.print(2, i->description, std::cout);
+                std::cout << std::endl;
+                ++beg;
+            }
+        }
+        T read() const
+        {
+            unsigned int sel;
+            cout << "Enter selection: ";
+            cout.flush();
+            cin >> sel;
+            while (size() <= sel)
+            {
+                cout << "No such configuration!" << endl;
+                cout << "Enter selection: ";
+                cout.flush();
+                cin >> sel;
+            }
+            return lst[sel];
+        }
+        T find(const std::string& s) const
+        {
+            const auto it = m.find(s);
+            return it == m.end() ? nullptr : lst[it->second];
+        }
+        size_t size() const
+        {
+            return lst.size();
+        }
+
+        std::vector<T> lst;
+
+    private:
+        std::map<std::string, size_t> m;
+        mutable out::table tab;
+    };
+
+    class compiler
+    {
+    public:
+        compiler(const char* name, const char* descript)
+            : name(name)
+            , description(descript)
+        {
+        }
+        virtual void init(const std::filesystem::path& dest);
+        virtual void update(const std::filesystem::path& dest);
+        virtual void deinit(const std::filesystem::path& dest);
+        virtual void read() {}
+        virtual ~compiler() {}
+
+        const std::string name, description;
+
+    protected:
+        virtual void initImpl(const std::filesystem::path& dest, bool upd = false) = 0;
+        virtual void deinitImpl(const std::filesystem::path& dest, bool upd = false) = 0;
+    };
+    list<compiler*>* shared();
+
+    class editor
+    {
+    public:
+        editor(const char* name, const char* description)
+            : name(name)
+            , description(description)
+            , com("Compiler")
+        {
+        }
+        virtual compiler* find(const std::string& c) const;
+        virtual compiler* read();
+        virtual void add(compiler* c);
+        virtual void print() const;
+        virtual void init(const std::filesystem::path& dest, compiler* c);
+        virtual void update(const std::filesystem::path& dest);
+        virtual void deinit(const std::filesystem::path& dest);
+        virtual ~editor() {}
+
+        const std::string name, description;
+
+    protected:
+        virtual void initImpl(const std::filesystem::path& dest, bool upd = false) = 0;
+        virtual void deinitImpl(const std::filesystem::path& dest, bool upd = false) = 0;
+
+        list<compiler*> com;
+    };
+    list<editor*>* editors();
+}
+#endif
---
+++ src/init/init-util.cpp
@@ -0,0 +1,94 @@
+#include "include/init.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace apdebug::init
+{
+    using std::cout;
+    using std::string;
+    using std::unique_ptr;
+    using std::filesystem::path;
+
+    list<compiler*>* shared()
+    {
+        static unique_ptr<list<compiler*>> obj(new list<compiler*>("Shared"));
+        return obj.get();
+    }
+    list<editor*>* editors()
+    {
+        static unique_ptr<list<editor*>> obj(new list<editor*>("Editors"));
+        return obj.get();
+    }
+
+    string readFileLn(const path& p)
+    {
+        string ret;
+        std::ifstream f(p);
+        std::getline(f, ret);
+        return ret;
+    }
+
+    compiler* editor::find(const string& c) const
+    {
+        compiler* ptr = com.find(c);
+        return ptr ? ptr : shared()->find(c);
+    }
+    compiler* editor::read()
+    {
+        unsigned int sel;
+        std::cout << "Enter compiler: ";
+        std::cout.flush();
+        std::cin >> sel;
+        while (sel >= com.size() && sel - com.size() > shared()->size())
+        {
+            std::cout << "No such config!" << std::endl;
+            std::cout << "Enter compiler: ";
+            std::cout.flush();
+            std::cin >> sel;
+        }
+        return sel >= com.size() ? shared()->lst[sel - com.size()] : com.lst[sel];
+    }
+    void editor::add(compiler* c)
+    {
+        com.append(c);
+    }
+    void editor::init(const path& dest, compiler* c)
+    {
+        initImpl(dest);
+        writeFile(dest / ".config" / "compiler", c->name);
+        c->init(dest);
+    }
+    void editor::print() const
+    {
+        com.print();
+        shared()->print(com.lst.size());
+    }
+    void editor::update(const path& dest)
+    {
+        deinitImpl(dest, true);
+        initImpl(dest, true);
+        find(readFileLn(dest / ".config" / "compiler"))->update(dest);
+    }
+    void editor::deinit(const path& dest)
+    {
+        com.find(readFileLn(dest / ".config" / "compiler"))->deinit(dest);
+        deinitImpl(dest);
+    }
+
+    void compiler::init(const path& dest)
+    {
+        initImpl(dest);
+    }
+    void compiler::update(const path& dest)
+    {
+        deinitImpl(dest, true);
+        initImpl(dest, true);
+    }
+    void compiler::deinit(const path& dest)
+    {
+        deinitImpl(dest);
+    }
+}
