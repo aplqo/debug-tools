bae5e33d378f2e1046d3b256a4ae972b492becef
aplqo <aplqo@outlook.com>
2020-05-30 18:10:09 +0800

    [group] Add support for nultigroup test.

--- src/grouptest.cpp
+++ src/grouptest.cpp
@@ -1,6 +1,7 @@
 #include "include/cmdarg.h"
 #include "include/define.h"
 #include "include/output.h"
+#include "include/regexseq.h"
 #include "include/testcase.h"
 #include <algorithm>
 #include <cmath>
@@ -9,9 +10,11 @@
 #include <functional>
 #include <iostream>
 #include <map>
-#include <regex>
+#include <memory>
 #include <string>
+#include <type_traits>
 #include <vector>
+using apdebug::regex_seq::RegexSeq;
 using apdebug::testcase::result;
 using apdebug::testcase::tpoint;
 using apdebug::timer::timType;
@@ -20,11 +23,6 @@ using namespace std::filesystem;
 using namespace apdebug::out;
 using namespace apdebug::args;
 
-table tab {
-    "Id", "State(Run)", "State(Test)", "Input",
-    "Output", "Answer", "Time(ms)", "Time(us)",
-    "Details"
-};
 enum cols
 {
     Id = 0,
@@ -44,9 +42,9 @@ public:
         : id(i)
     {
     }
-    void init();
+    void init(const result& exe, const result& tes);
     void exec();
-    void print();
+    void print(table& tab);
     void update_table(table& t);
     int id;
 
@@ -54,31 +52,30 @@ public:
     using tpoint::lim;
     using tpoint::release;
 
-    static result exe, tes;
+    static string cmd;
+    static unsigned int grpId;
     static bool verbose;
 
-    friend void getfiles(path indir, path ansdir, regex inreg, regex ansreg);
+    friend class group;
 
 private:
-    bool testen = true;
     void getOut();
 };
-result point::exe;
-result point::tes;
 bool point::verbose = false;
-void point::init()
+unsigned int point::grpId = 0;
+string point::cmd;
+void point::init(const result& exe, const result& tes)
 {
     this->rres = exe;
+    this->rres.cmd = cmd;
     this->tres = tes;
     getOut();
-    if (regex_search(tres.args, regex(R"(\[answer\])")) && ans.empty())
-        testen = false;
     this->tpoint::init();
 }
 void point::exec()
 {
     cout << endl;
-    cout << col::Underline << col::BLUE << "[Info] Start program for test #" << id << col::NONE;
+    cout << col::Underline << col::BLUE << "[Info] Start program for test #" << grpId << "." << id << col::NONE;
     if (verbose)
     {
         cout << col::CYAN << endl;
@@ -88,9 +85,9 @@ void point::exec()
     this->run();
     this->parse();
     cout << s->verbose();
-    if (success() && !tres.cmd.empty() && testen)
+    if (success() && !tres.cmd.empty() && !ans.empty())
     {
-        cout << col::BLUE << "[Info] Start testing for test #" << id;
+        cout << col::BLUE << "[Info] Start testing for test #" << grpId << "." << id;
         if (verbose)
         {
             cout << col::CYAN << endl;
@@ -100,9 +97,9 @@ void point::exec()
         this->test();
         cout << ts->verbose();
     }
-    cout << col::Underline << col::BLUE << "[Info] Test #" << id << " finished." << col::NONE << endl;
+    cout << col::Underline << col::BLUE << "[Info] Test #" << grpId << "." << id << " finished." << col::NONE << endl;
 }
-void point::print()
+void point::print(table& tab)
 {
     if (rres.ret || fail)
         cout << s->color();
@@ -146,143 +143,237 @@ void point::getOut()
     this->out = p.string();
 }
 
-vector<point> tests;
-regex testreg;
-
-void getfiles(path indir, path ansdir, regex inreg, regex ansreg)
+struct config
 {
-    static map<size_t, int> table;
-    static int cur = 0;
-    auto getHash = [](string s, regex reg) -> size_t {
-        s = regex_replace(s, regex(testreg), "");
-        s = regex_replace(s, reg, "");
-        return hash<string>()(s);
-    };
-    auto isInclude = [](string s, regex reg) -> bool {
-        smatch m1, m2;
-        return regex_search(s, m1, testreg) && regex_search(s, m2, reg);
-    };
-    {
+    shared_ptr<path> indir, ansdir;
+    bool inrec = false, ansrec = false, verbose = false;
+    shared_ptr<RegexSeq> rin, rans, rtest;
+    shared_ptr<result> exe, tes;
+    timType hlimit = 1000 * 1000, lim = 100 * 10 * 1000;
 
-        directory_iterator init(indir);
-        for (auto& i : init)
-        {
-            if (!i.is_regular_file())
-                continue;
-            string s = i.path().filename().string();
-            if (!isInclude(s, inreg))
-                continue;
-            tests.push_back(point(cur));
-            table[getHash(s, inreg)] = cur;
-            tests[cur].in = i.path().string();
-            ++cur;
-        }
-    }
+protected:
+    void read(int& pos, char* argv[]);
+};
+void config::read(int& pos, char* argv[])
+{
+    for (; true; ++pos)
     {
-        if (!exists(ansdir))
-            return;
-        directory_iterator ansit(ansdir);
-        for (auto& i : ansit)
+        if (!strcmp(argv[pos], "-test-regex"))
+            rtest = make_shared<RegexSeq>(++pos, argv);
+        else if (!strcmp(argv[pos], "-indir"))
         {
-            if (!i.is_regular_file())
-                continue;
-            string s = i.path().filename().string();
-            if (!isInclude(s, ansreg))
-                continue;
-            auto it = table.find(getHash(s, ansreg));
-            if (it == table.end())
-                continue;
-            tests[it->second].ans = i.path().string();
+            if (!strcmp(argv[pos + 1], "-R"))
+            {
+                ++pos;
+                inrec = true;
+            }
+            indir = make_shared<path>(argv[++pos]);
         }
+        else if (!strcmp(argv[pos], "-in-regex"))
+            rin = make_shared<RegexSeq>(++pos, argv);
+        else if (!strcmp(argv[pos], "-ansdir"))
+        {
+            if (!strcmp(argv[pos + 1], "-R"))
+            {
+                ++pos;
+                ansrec = true;
+            }
+            ansdir = make_shared<path>(argv[++pos]);
+        }
+        else if (!strcmp(argv[pos], "-ans-regex"))
+            rans = make_shared<RegexSeq>(++pos, argv);
+        else if (ReadLimit<point>(pos, argv))
+        {
+            hlimit = point::hardlim;
+            lim = point::lim;
+        }
+        else if (!strcmp(argv[pos], "-args"))
+        {
+            exe = make_shared<result>(*exe);
+            ReadArgument(*exe, ++pos, argv);
+        }
+        else if (!strcmp(argv[pos], "-test"))
+        {
+            if (tes.use_count() > 1)
+                tes = make_shared<result>(*tes);
+            else if (!tes)
+                tes = make_shared<result>();
+            tes->cmd = argv[++pos];
+        }
+        else if (!strcmp(argv[pos], "-testargs"))
+        {
+            if (tes.use_count() > 1)
+                tes = make_shared<result>(*tes);
+            else if (!tes)
+                tes = make_shared<result>();
+            ReadArgument(*tes, ++pos, argv);
+        }
+        else if (!strcmp(argv[pos], "-verbose"))
+            verbose = true;
+        else if (!strcmp(argv[pos], ";"))
+            break;
     }
+}
+class group : config
+{
+public:
+    group(unsigned int i, int& pos, char* argv[]);
+    group(const unsigned int c, unsigned int i, int& pos, char* argv[]);
+    void exec();
+    void findFile();
+    void print();
+
+private:
+    template <bool rec>
+    void findInput();
+    template <bool rec>
+    void findAnswer();
+    template <bool rec>
+    pair<bool, size_t> isInclude(const path& p, const RegexSeq& r);
+
+    table tab { "Id", "State(Run)", "State(Test)", "Input",
+        "Output", "Answer", "Time(ms)", "Time(us)",
+        "Details" };
+    unsigned int id = 0;
+    vector<point> tests;
+    map<size_t, int> table;
+};
+vector<group> grp;
+group::group(unsigned int i, int& pos, char* argv[])
+    : id(i)
+{
+    read(pos, argv);
+}
+group::group(const unsigned int c, unsigned int i, int& pos, char* argv[])
+    : config::config(grp[c])
+    , id(i)
+{
+    read(pos, argv);
+}
+void group::exec()
+{
+    const auto deref = [](shared_ptr<result>& s) { return s ? *s : result(); };
+    point::lim = lim;
+    point::hardlim = hlimit;
+    point::grpId = id;
+    cout << col::BLUE << col::Underline << "[Info] Start testing for group #" << id << col::NONE;
+    for (auto& i : tests)
+    {
+        i.init(deref(exe), deref(tes));
+        i.exec();
+        i.release();
+        i.update_table(tab);
+    }
+    cout << col::BLUE << col::Underline << "[Info] Group #" << id << " finished." << col::NONE << endl;
+    cout << endl;
+}
+void group::print()
+{
+    cout << col::NONE << "Test result for group #" << id << endl;
+    if (!tests.size())
+    {
+        cout << col::RED << "[Err] Can't find any test data." << col::NONE << endl;
+        return;
+    }
+    //print table
+    {
+        tab.update(Id, log10(tests.size()) + 2);
+        tab.update(MsTim, log10(hlimit / 1000) + 2);
+        tab.update(UsTim, log10(hlimit) + 2);
+        tab.header(cout);
+    }
+    for (auto& i : tests)
+        i.print(tab);
+    cout << col::NONE << endl;
+}
+void group::findFile()
+{
+    if (inrec)
+        findInput<true>();
+    else
+        findInput<false>();
+    if (ansrec)
+        findAnswer<true>();
+    else
+        findAnswer<false>();
+    table.clear();
+}
+template <bool rec>
+void group::findInput()
+{
+    unsigned int cur = 0;
+    conditional_t<rec, recursive_directory_iterator, directory_iterator> init(*indir);
+    for (auto& i : init)
+    {
+        if (!i.is_regular_file())
+            continue;
+        const auto [suc, hsh] = isInclude<rec>(i.path(), *rin);
+        if (!suc)
+            continue;
+        tests.push_back(point(cur));
+        table[hsh] = cur;
+        tests[cur].in = i.path().string();
+        ++cur;
+    }
+}
+template <bool rec>
+void group::findAnswer()
+{
+    if (!exists(*ansdir))
+        return;
+    conditional_t<rec, recursive_directory_iterator, directory_iterator> ansit(*ansdir);
+    for (auto& i : ansit)
+    {
+        if (!i.is_regular_file())
+            continue;
+        const auto [suc, hsh] = isInclude<rec>(i.path(), *rans);
+        if (!suc)
+            continue;
+        auto it = table.find(hsh);
+        if (it == table.end())
+            continue;
+        tests[it->second].ans = i.path().string();
+        table.erase(it);
+    }
+}
+template <bool rec>
+pair<bool, size_t> group::isInclude(const path& p, const RegexSeq& r)
+{
+    string s;
+    if constexpr (rec)
+        s = p.string();
+    else
+        s = p.filename().string();
+    const auto [suc1, val1] = rtest->eval(s);
+    if (!suc1)
+        return make_pair(false, 0);
+    const auto [suc2, val2] = r.eval(s);
+    if (!suc2)
+        return make_pair(false, 0);
+    return make_pair(true, hash<string>()(val1 + val2));
 }
 
 int main(int argc, char* argv[])
 {
     if (strcmp(argv[2], "-no-version"))
         PrintVersion("group test runner", cout);
-
-    path indir, ansdir;
-    regex inreg, ansreg;
-
-    point::exe.cmd = argv[1];
-    for (int i = 2; i < argc; ++i)
+    point::cmd = argv[1];
+    for (int i = 2, id = 0; i < argc; ++i)
     {
-        if (!strcmp(argv[i], "-test-regex"))
+        if (strcmp(argv[i], "-use"))
+            grp.emplace_back(id++, i, argv);
+        else
         {
-            cout << col::CYAN << "[Info] Test file regex: \'" << argv[++i] << "\'" << endl;
-            testreg = regex(argv[i]);
-            continue;
+            const unsigned int u = atoi(argv[++i]);
+            grp.emplace_back(u, id++, ++i, argv);
         }
-        if (!strcmp(argv[i], "-indir"))
-        {
-            cout << col::CYAN << "[Info] Input directory: " << argv[++i] << endl;
-            indir = argv[i];
-            continue;
-        }
-        if (!strcmp(argv[i], "-in-regex"))
-        {
-            cout << col::CYAN << "[Info] Input regex: \'" << argv[++i] << "\'" << endl;
-            inreg = regex(argv[i]);
-            continue;
-        }
-        if (!strcmp(argv[i], "-ansdir"))
-        {
-            cout << col::CYAN << "[Info] Answer directory: " << argv[++i] << endl;
-            ansdir = argv[i];
-            continue;
-        }
-        if (!strcmp(argv[i], "-ans-regex"))
-        {
-            cout << col::CYAN << "[Info] Answer regex: \'" << argv[++i] << "\'" << endl;
-            ansreg = regex(argv[i]);
-            continue;
-        }
-        if (!strcmp(argv[i], "-test"))
-            point::tes.cmd = argv[++i];
-        if (ReadLimit<point>(i, argv))
-            continue;
-        if (!strcmp(argv[i], "-args"))
-        {
-            cout << col::CYAN << "[Info] Arguments: ";
-            ReadArgument(point::exe, ++i, argv);
-            cout << endl;
-        }
-        if (!strcmp(argv[i], "-testargs"))
-        {
-            cout << col::CYAN << "[Info] Test command: ";
-            ReadArgument(point::tes, ++i, argv);
-            cout << point::tes.cmd << " " << point::tes.args << endl;
-        }
-        if (!strcmp(argv[i], "-verbose"))
-            point::verbose = true;
     }
-    cout << col::CYAN << "[Info] Verbose output: " << boolalpha << point::verbose << endl;
-    PrintLimit<point>(cout, false);
-    cout << col::NONE << endl;
-    getfiles(indir, ansdir, inreg, ansreg);
-    if (!tests.size())
+    for (auto& i : grp)
     {
-        cout << col::RED << "[Err] Can't find any test data." << col::NONE << endl;
-        return 1;
-    }
-    for (auto& i : tests)
-    {
-        i.init();
+        i.findFile();
         i.exec();
-        i.release();
-        i.update_table(tab);
     }
-    //print table
-    {
-        tab.update(Id, log10(tests.size()) + 2);
-        tab.update(MsTim, log10(point::hardlim / 1000) + 2);
-        tab.update(UsTim, log10(point::hardlim) + 2);
-        cout << col::NONE << endl
-             << "Test results:" << endl;
-        tab.header(cout);
-    }
-    for (auto& i : tests)
+    for (auto& i : grp)
         i.print();
     cout << col::NONE << endl;
     return 0;
