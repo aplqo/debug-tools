be6f83be97715fd527082f8393a26f5e81d1ed52
aplqo <aplqo@outlook.com>
2021-01-13 07:24:03 +0800

    [config] Linux: update script and vscode config

--- debug_scripts/shell/testall.sh
+++ debug_scripts/shell/testall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly group='./bin/group-tra'
+
 regex='^(.*)\.[^\.]*' # match without extension
 inreg='.*\.in$'
 ansreg='.*\.ans$'
@@ -16,4 +18,4 @@ if [ -n "$5" ]
 then
     ansreg="$5"
 fi
-./bin/group -program $1 -test-regex [ "$regex" -em 1 ';' ] -indir "$2" -in-regex [ "$inreg" -mm  ';' ] -ansdir "$2" -ans-regex [ "$ansreg" -mm ';' ] -test diff -test-args [ '{output}' '{answer}' -a -w -u --color ] ';'
+$group -program $1 -test-regex [ "$regex" -em 1 ';' ] -indir "$2" -in-regex [ "$inreg" -mm  ';' ] -ansdir "$2" -ans-regex [ "$ansreg" -mm ';' ] -test diff -test-args [ '{output}' '{answer}' -a -w -u --color ] ';'
--- debug_scripts/shell/testrnd.sh
+++ debug_scripts/shell/testrnd.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./bin/random -program $1 -tmpdir "$5" -generator "$2" -gen-args $3 -times $4 -stop-on-error -test diff -test-args [ "{output}" "{answer}" -a -w -u ]
+readonly random='./bin/random-tra'
+
+$random -program $1 -tmpdir "$5" -generator "$2" -gen-args $3 -times $4 -stop-on-error -test diff -test-args [ "{output}" "{answer}" -a -w -u ]
