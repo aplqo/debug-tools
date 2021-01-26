1a2b4d7d5005a8dc22a64c499b1b4272d4bcee1f
aplqo <aplqo@outlook.com>
2021-01-26 14:35:52 +0800

    build: add option to output LTO

--- build-tools.sh
+++ build-tools.sh
@@ -12,7 +12,7 @@ then
 	mkdir -p "$build"
 fi
 
-cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX="$2" -Dcolor=on -Dver="$1" -S "${src}" -B "$build"
+cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX="$2" -DoutputLTO=on -Dver="$1" -S "${src}" -B "$build"
 cd "$build"
 make
 make install
