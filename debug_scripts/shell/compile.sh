f24c6377c0444281f780eb9b14d6c0b6b88fb18d
aplqo <aplqo@outlook.com>
2021-01-26 16:50:37 +0800

    [config,scripts] Update compile method

--- debug_scripts/shell/compile.sh
+++ debug_scripts/shell/compile.sh
@@ -6,7 +6,7 @@ readonly args="-std=c++11 -Wall -Wextra -Weffc++ -Wdisabled-optimization"
 
 if [ "$2" = 'test' ]
 then
-	$CXX -DAPTEST "$fdir/$base.cpp" $args -Wl,--rpath=. -I. -L./lib -ljudge -lsystem_min -lrt -lbacktrace -ldl -o "$fdir/$base"
+	$CXX -DAPTEST "$fdir/$base.cpp" $args -Wl,--rpath=. -I. -L./lib -ljudge -lsystem_judge -lrt -lbacktrace -ldl -o "$fdir/$base"
 else
 	$CXX -DAPDEBUG $args "$fdir/$base.cpp" -g  -o "$fdir/$base"
 fi
