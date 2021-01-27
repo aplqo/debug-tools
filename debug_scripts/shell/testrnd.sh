eefaf6188497610ff55e1ac9b99c0e18c0b021c4
aplqo <aplqo@outlook.com>
2021-01-27 12:18:40 +0800

    [scripts] shell: Update testrnd.sh

--- debug_scripts/shell/testrnd.sh
+++ debug_scripts/shell/testrnd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-readonly random='./bin/random-tra'
+readonly program=''
+readonly gen=''
+readonly std=''
 
-./bin/random-$type -program $1 -tmpdir "$5" -generator "$2" -gen-args $3 -times $4 -stop-on-error -test diff -test-args [ "{output}" "{answer}" -a -w -u ]
+./bin/random-$type -program $program -tmpdir "$1" -generator $gen -gen-args [ '{input}' ] -standard $std -std-args [ '{input}' '{answer}' ] -times $4 -stop-on-error -test diff -test-args [ "{output}" "{answer}" -a -w -u ]
